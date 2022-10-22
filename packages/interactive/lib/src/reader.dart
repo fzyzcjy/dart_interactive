4344cb76a5510b6d90a9175780bb44086de7497a
fzyzcjy <ch271828n@outlook.com>
2022-10-22 18:19:07 +0800

    replValidator and ut

--- packages/interactive/lib/src/reader.dart
+++ packages/interactive/lib/src/reader.dart
@@ -3,6 +3,21 @@ import 'package:interactive/src/main.dart';
 
 Reader createReader() => Repl(prompt: '>>> ', validator: replValidator).run;
 
+const _leftBrackets = ['{', '[', '('];
+const _rightToLeftBracketMap = {'}': '{', ']': '[', ')': '('};
+
 bool replValidator(String text) {
-  return TODO;
+  final stack = <String>[];
+  for (var i = 0; i < text.length; ++i) {
+    final ch = text[i];
+    if (_leftBrackets.contains(ch)) {
+      stack.add(ch);
+    } else if (_rightToLeftBracketMap.containsKey(ch)) {
+      // not check matching pairs currently, since user can
+      // input a wrong grammar
+      if (stack.isNotEmpty) stack.removeLast();
+    }
+  }
+
+  return stack.isEmpty;
 }
---
+++ packages/interactive/test/reader_test.dart
@@ -0,0 +1,18 @@
+import 'package:interactive/src/reader.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('replValidator', () {
+    expect(replValidator('a = 1'), true);
+    expect(replValidator('f()'), true);
+    expect(replValidator('f(g())'), true);
+    expect(replValidator('void f() {}'), true);
+    expect(replValidator('a[1]'), true);
+
+    expect(replValidator('f('), false);
+    expect(replValidator('f(g('), false);
+    expect(replValidator('f(g()'), false);
+    expect(replValidator('void f() {'), false);
+    expect(replValidator('a[1'), false);
+  });
+}
