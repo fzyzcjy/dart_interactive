188a3b41d638765f826e5be16a7c3d8a8c1c778e
fzyzcjy <ch271828n@outlook.com>
2022-10-22 20:44:24 +0800

    minor

--- packages/interactive/lib/src/runtime_support.dart
+++ packages/interactive/lib/src/runtime_support.dart
@@ -1,18 +1,13 @@
 import 'dart:mirrors';
 
-Future<void> executionWorkspaceMain() async {
-  print('executionWorkspaceMain called and sleep');
-  await Future<void>.delayed(const Duration(days: 1000));
-}
+Future<void> executionWorkspaceMain() async =>
+    await Future<void>.delayed(const Duration(days: 1000));
 
 class InteractiveRuntimeContext {
   final _fieldMap = <Symbol, Object?>{};
 
   @override
   Object? noSuchMethod(Invocation invocation) {
-    // print(
-    //     'InteractiveRuntimeContext.noSuchMethod memberName=${invocation.memberName} positionalArguments=${invocation.positionalArguments} namedArguments=${invocation.namedArguments}');
-
     if (invocation.isGetter && _fieldMap.containsKey(invocation.memberName)) {
       return _fieldMap[invocation.memberName];
     }
@@ -38,10 +33,6 @@ class InteractiveRuntimeContext {
 
 // used by [execution_workspace], not by code *inside* [interactive]
 final interactiveRuntimeContext = InteractiveRuntimeContext();
-
-// convenient name for repl
-// notice this "dynamic" - otherwise cannot access fields
-final dynamic $ = interactiveRuntimeContext;
 
 dynamic synthesizedClassNoSuchMethod(Invocation invocation) =>
     interactiveRuntimeContext.noSuchMethod(invocation);
