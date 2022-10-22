0fee9588a163e50842341bc28241a3845943228b
fzyzcjy <ch271828n@outlook.com>
2022-10-22 15:39:37 +0800

    logging infra

--- packages/interactive/bin/interactive.dart
+++ packages/interactive/bin/interactive.dart
@@ -1,7 +1,3 @@
 import 'package:interactive/src/main.dart' as lib_main;
 
-// call via `dart bin/interactive.dart --enable-vm-service`
-void main() {
-  lib_main.main();
-}
-
+void main(List<String> args) => lib_main.main(args);
--- packages/interactive/lib/src/main.dart
+++ packages/interactive/lib/src/main.dart
@@ -1,21 +1,35 @@
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:interactive/src/parser.dart';
 import 'package:interactive/src/reader.dart';
 import 'package:interactive/src/vm_service_wrapper.dart';
 import 'package:interactive/src/workspace_code.dart';
 import 'package:interactive/src/workspace_isolate.dart';
+import 'package:logging/logging.dart';
 import 'package:vm_service/vm_service.dart';
 
 // TODO should dynamically generate and do not hardcode path...
 const executionWorkspaceDir =
     '/Users/tom/RefCode/dart_interactive/packages/execution_workspace';
 
-Future<void> main() => run(reader: ReplReader());
+Future<void> main(List<String> args) {
+  final parsedArgs = (ArgParser() //
+        ..addFlag('verbose', defaultsTo: false))
+      .parse(args);
+
+  return run(
+    reader: ReplReader(),
+    verbose: parsedArgs['verbose'] as bool,
+  );
+}
 
 Future<void> run({
+  required bool verbose,
   required BaseReader reader,
 }) async {
+  _setUpLogging(verbose ? Level.ALL : Level.WARNING);
+
   final vm = await VmServiceWrapper.create();
   final executionWorkspaceManager =
       await WorkspaceIsolate.create(vm, executionWorkspaceDir);
@@ -72,4 +86,11 @@ void _handleEvaluateResponse(Response response) {
   } else {
     print('Unknown error (response: $response)');
   }
+}
+
+void _setUpLogging(Level level) {
+  Logger.root
+    ..level = level
+    ..onRecord.listen((record) =>
+        print('[${record.level.name} ${record.time}] ${record.message}'));
 }
