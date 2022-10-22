45051096112aeb5a74355d6083e11ce0d3b30396
fzyzcjy <ch271828n@outlook.com>
2022-10-22 13:23:23 +0800

    add error listener

--- packages/interactive/lib/src/execution_workspace_manager.dart
+++ packages/interactive/lib/src/execution_workspace_manager.dart
@@ -17,11 +17,29 @@ class ExecutionWorkspaceManager {
 
     final isolateIdsBefore = vm.vm.isolates!.map((e) => e.id).toSet();
 
-    final isolate = await Isolate.spawnUri(Uri.file(path), const [], null);
+    final isolate = _spawnUriWithErrorHandling(Uri.file(path));
 
     final isolateIdsAfter = vm.vm.isolates!.map((e) => e.id).toSet();
+    print('isolateIdsAfter=$isolateIdsAfter');
     final isolateId = isolateIdsAfter.difference(isolateIdsBefore).single!;
 
     return ExecutionWorkspaceManager._(isolate: isolate, isolateId: isolateId);
   }
+}
+
+// ref: [Isolate.run]
+Future<Isolate> _spawnUriWithErrorHandling(Uri uri) async {
+  final errorPort = RawReceivePort()
+    ..handler = (Object? message) => print('Isolate error: $message');
+  final exitPort = RawReceivePort()
+    ..handler =
+        (Object? message) => print('Isolate exited (message: $message)');
+
+  return await Isolate.spawnUri(
+    uri,
+    const [],
+    null,
+    onError: errorPort.sendPort,
+    onExit: exitPort.sendPort,
+  );
 }
