c16099fe2523211bfc2fd5598d103b639e9a7c44
fzyzcjy <ch271828n@outlook.com>
2022-10-22 15:55:52 +0800

    add simplest e2e test

--- packages/interactive/test/e2e_test.dart
+++ packages/interactive/test/e2e_test.dart
@@ -1,3 +1,20 @@
+import 'package:interactive/src/main.dart';
+import 'package:test/test.dart';
+
 void main() {
-  // TODO
+  test(
+      'simplest', () => _body(inputs: ['print("hi");'], expectOutputs: ['hi']));
+}
+
+Future<void> _body({
+  required List<String> inputs,
+  required List<String> expectOutputs,
+}) async {
+  final actualOutputs = <String>[];
+  await run(
+    verbose: true,
+    reader: () => inputs,
+    writer: actualOutputs.add,
+  );
+  expect(actualOutputs, expectOutputs);
 }
