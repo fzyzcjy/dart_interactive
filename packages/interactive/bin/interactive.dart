12f6ea012055f644492915259ea990fe7efffde1
fzyzcjy <ch271828n@outlook.com>
2022-10-23 07:23:14 +0800

    try to wrap enable-vm-service

--- packages/interactive/bin/interactive.dart
+++ packages/interactive/bin/interactive.dart
@@ -1,3 +1,26 @@
-import 'package:interactive/src/main.dart' as lib_main;
+import 'dart:io';
 
-void main(List<String> args) => lib_main.main(args);
+// this file should have at few imports as possible to speed up booting
+Future<void> main(List<String> rawArgs) async {
+  final executable = Platform.executable;
+  final arguments = ['--enable-vm-service', _interactiveRawScript, ...rawArgs];
+
+  print('Run: $executable $arguments');
+  final process = await Process.start(executable, arguments,
+      mode: ProcessStartMode.inheritStdio);
+  final innerExitCode = await process.exitCode;
+  exit(innerExitCode);
+}
+
+// ref: https://github.com/dart-lang/pub/issues/3291
+String get _interactiveRawScript {
+  const kReplaceSrc = 'interactive.dart';
+  const kReplaceDst = 'interactive_raw.dart';
+
+  final self = Platform.script.toString();
+  if (!self.contains(kReplaceSrc)) {
+    throw AssertionError(
+        'self=$self does not contain $kReplaceSrc, please create an issue');
+  }
+  return self.replaceAll(kReplaceSrc, kReplaceDst);
+}
---
+++ packages/interactive/bin/interactive_raw.dart
@@ -0,0 +1,3 @@
+import 'package:interactive/src/main.dart' as lib_main;
+
+void main(List<String> args) => lib_main.main(args);
