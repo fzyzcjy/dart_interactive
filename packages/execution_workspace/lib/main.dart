7479adfd2d048bd1722c57b04a95ebe5d75da942
fzyzcjy <ch271828n@outlook.com>
2022-10-22 13:40:32 +0800

    add context

--- packages/execution_workspace/lib/main.dart
+++ packages/execution_workspace/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:interactive/src/runtime_support.dart';
+export 'package:interactive/src/runtime_support.dart';
 
 import 'workspace.dart'; // ignore: unused_import
 export 'workspace.dart';
--- packages/interactive/lib/src/runtime_support.dart
+++ packages/interactive/lib/src/runtime_support.dart
@@ -1,4 +1,37 @@
+import 'dart:mirrors';
+
 Future<void> executionWorkspaceMain() async {
   print('executionWorkspaceMain called and sleep');
   await Future<void>.delayed(const Duration(days: 1000));
 }
+
+class Context {
+  final _fieldMap = <Symbol, Object?>{};
+
+  @override
+  Object? noSuchMethod(Invocation invocation) {
+    if (invocation.isGetter && _fieldMap.containsKey(invocation.memberName)) {
+      return _fieldMap[invocation.memberName];
+    }
+
+    if (invocation.isSetter) {
+      final name = MirrorSystem.getSymbol(
+          MirrorSystem.getName(invocation.memberName).split('=').first);
+      _fieldMap[name] = invocation.positionalArguments.single;
+      return null;
+    }
+
+    if (invocation.isMethod) {
+      return Function.apply(_fieldMap[invocation.memberName]! as Function,
+          invocation.positionalArguments, invocation.namedArguments);
+    }
+
+    return super.noSuchMethod(invocation);
+  }
+
+  @override
+  String toString() => 'Context(fieldMap: $_fieldMap)';
+}
+
+// used by [execution_workspace], not by code *inside* [interactive]
+final context = Context();
