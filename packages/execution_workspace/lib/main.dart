9d9f0d04a2c5326a4984737620d0bc17bed680ae
fzyzcjy <ch271828n@outlook.com>
2022-10-22 20:39:36 +0800

    rm workspace.dart

--- packages/execution_workspace/lib/main.dart
+++ packages/execution_workspace/lib/main.dart
@@ -4,7 +4,4 @@ export 'package:interactive/src/runtime_support.dart';
 import 'auto_generated.dart'; // ignore: unused_import
 export 'auto_generated.dart';
 
-import 'workspace.dart'; // ignore: unused_import
-export 'workspace.dart';
-
 void main() => executionWorkspaceMain();
--- packages/interactive/lib/src/workspace_code.dart
+++ packages/interactive/lib/src/workspace_code.dart
@@ -46,9 +46,6 @@ class WorkspaceCode {
 
 import 'package:interactive/src/runtime_support.dart';
 
-import 'workspace.dart'; // ignore: unused_import
-export 'workspace.dart';
-
 ${imports.join('\n')}
 
 ${classMap.values.map((e) => e.generate()).join('\n\n')}
