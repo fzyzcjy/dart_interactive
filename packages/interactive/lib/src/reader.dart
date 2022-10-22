dfb2b8cede8a4a70f17b68786163f5a162a2f620
fzyzcjy <ch271828n@outlook.com>
2022-10-22 13:33:11 +0800

    mimic python

--- packages/interactive/lib/src/reader.dart
+++ packages/interactive/lib/src/reader.dart
@@ -1,7 +1,7 @@
 import 'package:cli_repl/cli_repl.dart';
 
 Future<void> runReader(Future<void> Function(String input) handler) async {
-  final repl = Repl(prompt: '> ');
+  final repl = Repl(prompt: '>>> ');
   for (final input in repl.run()) {
     if (input.trim().isEmpty) continue;
     await handler(input);
